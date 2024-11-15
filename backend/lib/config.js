777dec48210620a25b50e0e5662526b1fea66d2a
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-11-16 05:44:25 +0700

    Flare Addresses Added

--- backend/lib/config.js
+++ backend/lib/config.js
@@ -5,52 +5,57 @@ const EqualUnionabi = require("./contracts/EqualUnion.json");
 const NFTUnionabi = require("./contracts/NFTUnion.json");
 const TraditionalUnionabi = require("./contracts/TraditionalUnion.json");
 const QuadraticUnionabi = require("./contracts/QuadraticUnion.json");
+const FlareUnionabi = require("./contracts/FlareUnion.json");
 
 const config = {
   chains: [
     {
-      name: "Base Sepolia",
-      chainId: 1,
-      rpcUrl: "https://base-sepolia.gateway.tenderly.co/1LSwOzY43Ez2ZYqBL6U0xZ",
+      name: "Flare Testnet",
+      chainId: 114,
+      rpcUrl: "https://coston2-explorer.flare.network",
       deployments: {
         DAO: {
           address: "",
           abi: DAOabi,
         },
         UnionProxyFactory: {
-          address: "0x3705505C5690a836b33736CD13568Ee8700D35c4",
+          address: "0x0B62BDA8EcE17AFfa7adAe16bBaBBC8584A30016",
           abi: UnionProxyFactoryabi,
         },
         ConvictionUnion: {
-          address: "0x40c47752fcB1CA46D1b6Db5d715f5028f2a0Cb64",
+          address: "0x40C92d2E370b3d3944fDd90c922a407F02D286d1",
           abi: ConvictionUnionabi,
         },
         EqualUnion: {
-          address: "0x979602c7512024c01c14D42Bd27865886Ff38Dca",
+          address: "0x44950f083691828A07c17d2A927B435e8B272F6D",
           abi: EqualUnionabi,
         },
         NFTUnion: {
-          address: "0x3C7A418F01aA794C004cc6ba1Ad22a7eEA8BE232",
+          address: "0x1275917daAE6389C61c7B1E8199724D0b46Ed10f",
           abi: NFTUnionabi,
         },
         TraditionalUnion: {
-          address: "0x969ef16DbFb73c5eB2191CBF3632779C741750eE",
+          address: "0x0c17B9e142a0DDb42f075b0E2f1988691ea1d75a",
           abi: TraditionalUnionabi,
         },
         QuadraticUnion: {
-          address: "0x4bB1161032dd6400767535e3f9A5dd36d52bd58b",
+          address: "0xC4e20B3BD1922A02c6DAC500De5754E3CC4046BB",
           abi: QuadraticUnionabi,
+        },
+        FlareUnion: {
+          address: "0xE34906Eda6Cf2cafFa1d567523C7954fDf9E80E0",
+          abi: FlareUnionabi,
         },
       },
       DAOs: [
         {
-          address: "0x63949B7b906417c555136028391699E2B5adb381",
+          address: "0xd17Dd62290EcdEa48e5029a4fBd519245C911c19",
           name: "FlareDAO",
         },
       ],
       NFT: [
         {
-          address: "0x4b0b59344B8Aca93771a1de418981fA80A356B92",
+          address: "0x614ae60954f0AEdd172141A9C52052a8B422DEfd",
           name: "NounsDAO",
         },
       ],
