66761b58dac56fa7e49eadf7e182c864ff8a6516
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-11-17 04:04:23 +0700

    Added Rootstock and Zircuit Testnet

--- backend/lib/config.js
+++ backend/lib/config.js
@@ -366,6 +366,108 @@ const config = {
         },
       ],
     },
+    {
+      name: "Rsk Testnet",
+      chainId: 31,
+      rpcUrl: "https://public-node.testnet.rsk.co",
+      deployments: {
+        DAO: {
+          address: "",
+          abi: DAOabi,
+        },
+        UnionProxyFactory: {
+          address: "0x40C92d2E370b3d3944fDd90c922a407F02D286d1",
+          abi: UnionProxyFactoryabi,
+        },
+        ConvictionUnion: {
+          address: "0x44950f083691828A07c17d2A927B435e8B272F6D",
+          abi: ConvictionUnionabi,
+        },
+        EqualUnion: {
+          address: "0x1275917daAE6389C61c7B1E8199724D0b46Ed10f",
+          abi: EqualUnionabi,
+        },
+        NFTUnion: {
+          address: "0x2fAF5856C60C14730fd7594684C2C7d52097f440",
+          abi: NFTUnionabi,
+        },
+        TraditionalUnion: {
+          address: "0x50F1bbb486D62921eD9cE411c6b85Ec0B73D9130",
+          abi: TraditionalUnionabi,
+        },
+        QuadraticUnion: {
+          address: "0xb52c96C52F83F0741b1EEb1Cb6c1b73C57bDEb7B",
+          abi: QuadraticUnionabi,
+        },
+        FlareUnion: {
+          address: "0xC1AEA53f0c1EF35219A90Bec9495D3561a4B5951",
+          abi: FlareUnionabi,
+        },
+      },
+      DAOs: [
+        {
+          address: "0x0B62BDA8EcE17AFfa7adAe16bBaBBC8584A30016",
+          name: "RskDAO",
+        },
+      ],
+      NFT: [
+        {
+          address: "0xd0958A708F6347bb07B877a75Ff7E402116Ce089",
+          name: "NounsDAO",
+        },
+      ],
+    },
+    {
+      name: "Zircuit Testnet",
+      chainId: 48899,
+      rpcUrl: "https://zircuit1-testnet.p2pify.com",
+      deployments: {
+        DAO: {
+          address: "",
+          abi: DAOabi,
+        },
+        UnionProxyFactory: {
+          address: "0x0B62BDA8EcE17AFfa7adAe16bBaBBC8584A30016",
+          abi: UnionProxyFactoryabi,
+        },
+        ConvictionUnion: {
+          address: "0x40C92d2E370b3d3944fDd90c922a407F02D286d1",
+          abi: ConvictionUnionabi,
+        },
+        EqualUnion: {
+          address: "0x44950f083691828A07c17d2A927B435e8B272F6D",
+          abi: EqualUnionabi,
+        },
+        NFTUnion: {
+          address: "0x1275917daAE6389C61c7B1E8199724D0b46Ed10f",
+          abi: NFTUnionabi,
+        },
+        TraditionalUnion: {
+          address: "0x0c17B9e142a0DDb42f075b0E2f1988691ea1d75a",
+          abi: TraditionalUnionabi,
+        },
+        QuadraticUnion: {
+          address: "0xC4e20B3BD1922A02c6DAC500De5754E3CC4046BB",
+          abi: QuadraticUnionabi,
+        },
+        FlareUnion: {
+          address: "0xC1AEA53f0c1EF35219A90Bec9495D3561a4B5951",
+          abi: FlareUnionabi,
+        },
+      },
+      DAOs: [
+        {
+          address: "0xd17Dd62290EcdEa48e5029a4fBd519245C911c19",
+          name: "ZircuitDAO",
+        },
+      ],
+      NFT: [
+        {
+          address: "0xE34906Eda6Cf2cafFa1d567523C7954fDf9E80E0",
+          name: "NounsDAO",
+        },
+      ],
+    },
   ],
 };
 
