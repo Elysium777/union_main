87574fb4e6780638db549b7f6aa6441140e3dec8
JA <ja@JAdeMacBook-Pro.local>
2024-11-30 15:49:04 +0800

    host

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        // 启用 Source Map 支持
+        if (!isServer) {
+            config.devtool = 'source-map';
+        }
+
+        // 确保 Webpack 不解析 `.d.ts` 文件
+        config.module.rules.push({
+            test: /\.d\.ts$/,
+            use: 'null-loader',
+        });
+        // 忽略 .map 文件的解析错误
+        config.module.rules.push({
+            test: /\.map$/,
+            use: ['source-map-loader'],
+            enforce: 'pre',
+        });
+
+        return config;
+    }
+};
 
 export default nextConfig;
